96a562d63be87a8676648c25622ceaf1f6bf3c9f
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-11-24 23:31:56 +0500

    Task3

---
+++ Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2.Lib
+{
+    public class DataService : ISprint5Task3V2
+    {
+        public string SaveToFileTextData(int x)
+        {
+            //string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask3.bin";
+            string path1 = Path.GetTempPath();
+            string path2 = "OutPutFileTask3.bin";
+            string path = Path.Combine(path1, path2);
+            FileInfo fileInfo = new FileInfo(path);
+
+            double y = (Math.Pow(Math.E, x)) / x;
+            y = Math.Round(y, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2.Lib;
+using System.IO;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //string path = @"D:\programming\repos\Tyuiu.DyuvenzhiMI.Sprint5\Tyuiu.DyuvenzhiMI.Sprint5.Task3.V2\bin\Debug\net8.0\OutPutFileTask3.bin";
+            string path1 = Path.GetTempPath();
+            string path2 = "OutPutFileTask3.bin";
+            string path = Path.Combine(path1, path2);
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
