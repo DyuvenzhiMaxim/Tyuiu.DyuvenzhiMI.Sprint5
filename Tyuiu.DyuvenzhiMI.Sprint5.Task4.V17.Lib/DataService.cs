9041360ca0f91efdf822b65d45b6633aaa8c35ae
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-11-25 00:01:34 +0500

    Task4

---
+++ Tyuiu.DyuvenzhiMI.Sprint5.Task4.V17.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint5.Task4.V17.Lib
+{
+    public class DataService : ISprint5Task4V17
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            double x = Convert.ToDouble(strX);
+            double y = Math.Sin(2 / (3 * x)) + Math.Pow(x, 2);
+            y = Math.Round(y);
+            return y;
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint5.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint5.Task4.V17.Lib;
+using System.IO;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint5.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V17.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
